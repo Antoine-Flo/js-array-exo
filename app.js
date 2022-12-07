ead3367a5cdea1422ffdfdcffc47f4dc239da394
Pauline de Valance <pauline.de.valance@gmail.com>
2022-12-07 16:11:38 +0100

    modification writerFile

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-const data = require('./data.json')
-const fs = require('fs')
+const data = require("./data.json");
+const fs = require("fs");
 
-const FILE_NAME = 'example'
+const FILE_NAME = "bronze";
 // What you put in the variable 'result' is what will be writen in the file
 // Change the variable FILE_NAME to create a new file with the result for each exercice
 // Comment out your solution once it's done to keep it as a reference
-
-
 
 // Example :
 // keep only the id and the username
@@ -14,11 +12,13 @@ const result = data.map((x) => {
   return {
     id: x.id,
     username: x.username,
-  }
-})
+  };
+});
 
 fs.writeFile(`./results/${FILE_NAME}.json`, JSON.stringify(result), (err) => {
   if (err) {
-    console.error(err)
+    console.error(err);
+  } else {
+    console.log("File written succefully");
   }
-})
+});
