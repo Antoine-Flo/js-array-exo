7e02e38641b675cec48398042249d5c4634bbaea
Pauline de Valance <pauline.de.valance@gmail.com>
2022-12-09 08:19:53 +0100

    exercice one finished

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const FILE_NAME = "bronze";
 // Change the variable FILE_NAME to create a new file with the result for each exercice
 // Comment out your solution once it's done to keep it as a reference
 
-// Example :
-// keep only the id and the username
+const result = usersList.map((user) => {
+  return {
+    id: user.id,
+    avatarId: user.avatar,
+  };
+});
 
-const result = usersList.filter((user) => user.subscription.plan === "Bronze");
+//const result = usersList.filter((user => user.id)
 
 fs.writeFile(`./results/${FILE_NAME}.json`, JSON.stringify(result), (err) => {
   if (err) {
@@ -18,3 +22,6 @@ fs.writeFile(`./results/${FILE_NAME}.json`, JSON.stringify(result), (err) => {
     console.log("File written succefully");
   }
 });
+
+//Exercice 1
+//const result = usersList.filter((user) => user.subscription.plan === "Bronze");
