308ec91f15556cdd346a5c8f145ffa46dc8ddfcf
PaulinedeValance <pauline.de.valance@gmail.com>
2023-01-04 10:37:54 +0100

    exercice avatarId

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const usersList = require("./data.json");
 const fs = require("fs");
 
-const FILE_NAME = "bronze";
+const FILE_NAME = "avatarId";
 // What you put in the variable 'result' is what will be writen in the file
 // Change the variable FILE_NAME to create a new file with the result for each exercice
 // Comment out your solution once it's done to keep it as a reference
 
 const result = usersList.map((user) => {
+  const url = new URL(user.avatar)
   return {
     id: user.id,
-    avatarId: user.avatar,
+    avatarId: url.pathname.replace(".png", "").replace("/", ""),
   };
+
 });
 
-//const result = usersList.filter((user => user.id)
+// const result = usersList.filter((user => user.id))
 
-fs.writeFile(`./results/${FILE_NAME}.json`, JSON.stringify(result), (err) => {
+fs.writeFile(`./results/${FILE_NAME}.json`, JSON.stringify(result, null, 4), (err) => {
   if (err) {
     console.error(err);
   } else {
