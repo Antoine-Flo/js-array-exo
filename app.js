cb4566130d12a288b9dd45d20fce632a11278816
PaulinedeValance <pauline.de.valance@gmail.com>
2023-01-04 16:11:26 +0100

    exercice 3

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 const usersList = require("./data.json");
 const fs = require("fs");
 
-const FILE_NAME = "avatarId";
+const FILE_NAME = "parsedAdress";
 // What you put in the variable 'result' is what will be writen in the file
 // Change the variable FILE_NAME to create a new file with the result for each exercice
 // Comment out your solution once it's done to keep it as a reference
 
-const result = usersList.map((user) => {
-  const url = new URL(user.avatar)
-  return {
-    id: user.id,
-    avatarId: url.pathname.replace(".png", "").replace("/", ""),
-  };
-
-});
-
-// const result = usersList.filter((user => user.id))
+const result = usersList.map(({ first_name, last_name, address }) => ({
+  name: `${first_name} ${last_name}`,
+  adress: `${address.street_address} ${address.zip_code} ${address.country}`
+}));
 
 fs.writeFile(`./results/${FILE_NAME}.json`, JSON.stringify(result, null, 4), (err) => {
   if (err) {
@@ -27,3 +21,14 @@ fs.writeFile(`./results/${FILE_NAME}.json`, JSON.stringify(result, null, 4), (er
 
 //Exercice 1
 //const result = usersList.filter((user) => user.subscription.plan === "Bronze");
+
+//Exercice 2
+// const result = usersList.map((user) => {
+//   const url = new URL(user.avatar)
+//   return {
+//     id: user.id,
+//     avatarId: url.pathname.replace(".png", "").replace("/", ""),
+//   };
+
+// });
+
